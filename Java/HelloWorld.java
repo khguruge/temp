17ff7b5272b59e86d8626a8c3fb0ef5f32405e3e
khguruge <gkasunharshana@gmail.com>
2020-08-24 15:02:47 +0530

    hello

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,7 +1,14 @@
 class HelloWorld 
 { 
 	public static void main(String args[]) 
-	{ 
-		System.out.println("Hello, World"); 
+	{ 	
+		for (int i = 0; i < 5; i++) {
+		System.out.println(i);
+	  }
+		System.out.println("Hello, World");
+
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		System.out.println(cars[0]);
+		
 	} 
 } 
