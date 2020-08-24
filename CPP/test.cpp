43d429321ac4c08c811ac6a59055844c5c54fdba
khguruge <gkasunharshana@gmail.com>
2020-08-24 17:59:22 +0530

    hello

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -1,8 +1,18 @@
-// Your First C++ Program
-
 #include <iostream>
+using namespace std;
+
+int plusFuncInt(int x, int y) {
+  return x + y;
+}
+
+double plusFuncDouble(double x, double y) {
+  return x + y;
+}
 
 int main() {
-    std::cout << "Hello World!";
-    return 0;
+  int myNum1 = plusFuncInt(8, 5);
+  double myNum2 = plusFuncDouble(4.3, 6.26);
+  cout << "Int: " << myNum1 << "\n";
+  cout << "Double: " << myNum2;
+  return 0;
 }
